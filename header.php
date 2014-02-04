ce2678584401a51d6aced8fb5d2b9d80050f6064
Rose Mckeon <shmoo@rozeykex.com>
2014-02-04 10:37:30 +0000

    Adjusted Top Bar Markup
    added data-topbar to nav element and cleaned up wp_nav_menu() functions.

--- header.php
+++ header.php
@@ -24,26 +24,18 @@
 <body <?php body_class(); ?>>
       
       <header id="site-header">
-		<nav class="top-bar" role="navigation">
+	<nav class="top-bar" data-topbar role="navigation">
             <ul class="title-area">
                 <li class="name">
-                    <h1 class="site-title">
-				      	<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			      	</h1>
+                    <h1 class="site-title"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                 </li>          
                 <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
             </ul>
             <section class="top-bar-section">
 	            <?php wp_nav_menu(array( 
 			        'container' => false,                           // remove nav container
-			        'container_class' => '',           			    // class of container
-			        'menu' => '',                      	        	// menu name
-			        'menu_class' => 'top-bar-menu left',         	// adding custom nav class
+			        'menu_class' => 'left',         		// adding custom nav class
 			        'theme_location' => 'primary-left',             // where it's located in the theme
-			        'before' => '',                                 // before each link <a> 
-			        'after' => '',                                  // after each link </a>
-			        'link_before' => '',                            // before each link text
-			        'link_after' => '',                             // after each link text
 			        'depth' => 5,                                   // limit the depth of the nav
 			    	'fallback_cb' => false,                         // fallback function (see below)
 			        'walker' => new guts_top_bar_walker()
@@ -51,14 +43,8 @@
 
                 <?php wp_nav_menu(array( 
 			        'container' => false,                           // remove nav container
-			        'container_class' => '',           				// class of container
-			        'menu' => '',                      	        	// menu name
-			        'menu_class' => 'top-bar-menu right',         	// adding custom nav class
+			        'menu_class' => 'right',         		// adding custom nav class
 			        'theme_location' => 'primary',                  // where it's located in the theme
-			        'before' => '',                                 // before each link <a> 
-			        'after' => '',                                  // after each link </a>
-			        'link_before' => '',                            // before each link text
-			        'link_after' => '',                             // after each link text
 			        'depth' => 5,                                   // limit the depth of the nav
 			    	'fallback_cb' => false,                         // fallback function (see below)
 			        'walker' => new guts_top_bar_walker()
