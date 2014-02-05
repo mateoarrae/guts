061c3bc4f65442c5e578259d51d3b0a4fa4b8fc5
Rose <shmoo@rozeykex.com>
2014-02-05 21:51:33 +0000

    removed trailing slases form meta tags

--- header.php
+++ header.php
@@ -12,9 +12,9 @@
 <!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
 <!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta name="viewport" content="width=device-width" />
+  <meta name="viewport" content="width=device-width">
   <title><?php wp_title( '|', true, 'right' ); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php wp_head(); ?>
