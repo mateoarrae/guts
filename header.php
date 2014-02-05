4ae02d2c8ec44da39da18480cd4375f5685a6b02
Rose <shmoo@rozeykex.com>
2014-02-05 21:48:42 +0000

    cleaned up and matched to boilerplate

--- header.php
+++ header.php
@@ -7,60 +7,46 @@
  * @since Guts 0.0.1
  */
 ?><!DOCTYPE html>
-<!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
-
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width" />
   <title><?php wp_title( '|', true, 'right' ); ?></title>
-  <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
   <?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?>>
-      
-      <header id="site-header">
-	<nav class="top-bar" data-topbar role="navigation">
-            <ul class="title-area">
-                <li class="name">
-                    <h1 class="site-title"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                </li>          
-                <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-            </ul>
-            <section class="top-bar-section">
-	            <?php wp_nav_menu(array( 
-			        'container' => false,                           // remove nav container
-			        'menu_class' => 'left',         		// adding custom nav class
-			        'theme_location' => 'primary-left',             // where it's located in the theme
-			        'depth' => 5,                                   // limit the depth of the nav
-			    	'fallback_cb' => false,                         // fallback function (see below)
-			        'walker' => new guts_top_bar_walker()
-				)); ?>
+<!--[if lt IE 7]>
+  <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
+<div class="l-header-container">
+  <header id="site-header">
+    <nav class="top-bar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+          <h1 class="site-title"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        </li>          
+        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+      </ul>
+      <section class="top-bar-section">
+        <?php wp_nav_menu(array('container' => false, 'menu_class' => 'left', 'theme_location' => 'primary-left', 'fallback_cb' => false, 'walker' => new guts_top_bar_walker() )); ?>
 
-                <?php wp_nav_menu(array( 
-			        'container' => false,                           // remove nav container
-			        'menu_class' => 'right',         		// adding custom nav class
-			        'theme_location' => 'primary',                  // where it's located in the theme
-			        'depth' => 5,                                   // limit the depth of the nav
-			    	'fallback_cb' => false,                         // fallback function (see below)
-			        'walker' => new guts_top_bar_walker()
-				)); ?>
-            </section>
-        </nav>
-		<div class="row">
-			<div class="large-12 columns">   
-				<h1 class="site-title">
-			      	<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-			      		<?php bloginfo( 'name' ); ?>
-			      	</a>
-			      	<small><?php bloginfo( 'description' ); ?></small>
-		      	</h1>
-	    </div>
+        <?php wp_nav_menu(array('container' => false, 'menu_class' => 'right', 'theme_location' => 'primary', 'walker' => new guts_top_bar_walker() )); ?>
+      </section>
+    </nav>
+    <div class="row">
+	  <div class="large-8 columns">   
+	    <h1 class="site-title"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> <small><?php bloginfo( 'description' ); ?></small></h1>
 	  </div>
-	  </header>
-
+	  <div class="large-3 offset-1 columns">
+	    <?php get_search_form(); ?>
+	  </div>
+    </div>
+  </header>
 <hr />
+</div>
