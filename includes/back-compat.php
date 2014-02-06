aca97aebde5cee084e5c40cbc7b2b025b13a379e
Rose <shmoo@rozeykex.com>
2014-02-06 21:59:31 +0000

    Made sure all references are to Guts

--- includes/back-compat.php
+++ includes/back-compat.php
@@ -1,71 +1,71 @@
 <?php
 /**
- * Twenty Thirteen back compat functionality
+ * Guts back compat functionality - Taken from Twenty Thirteen.
  *
- * Prevents Twenty Thirteen from running on WordPress versions prior to 3.6,
+ * Prevents Guts from running on WordPress versions prior to 3.6,
  * since this theme is not meant to be backward compatible and relies on
  * many new functions and markup changes introduced in 3.6.
  *
  * @package WordPress
- * @subpackage Twenty_Thirteen
- * @since Twenty Thirteen 1.0
+ * @subpackage Guts
+ * @since Guts 0.0.1
  */
 
 /**
- * Prevent switching to Twenty Thirteen on old versions of WordPress.
+ * Prevent switching to Guts on old versions of WordPress.
  *
  * Switches to the default theme.
  *
- * @since Twenty Thirteen 1.0
+ * @since Guts 0.0.1
  *
  * @return void
  */
-function twentythirteen_switch_theme() {
+function guts_switch_theme() {
 	switch_theme( WP_DEFAULT_THEME, WP_DEFAULT_THEME );
 	unset( $_GET['activated'] );
-	add_action( 'admin_notices', 'twentythirteen_upgrade_notice' );
+	add_action( 'admin_notices', 'guts_upgrade_notice' );
 }
-add_action( 'after_switch_theme', 'twentythirteen_switch_theme' );
+add_action( 'after_switch_theme', 'guts_switch_theme' );
 
 /**
  * Add message for unsuccessful theme switch.
  *
  * Prints an update nag after an unsuccessful attempt to switch to
- * Twenty Thirteen on WordPress versions prior to 3.6.
+ * Guts on WordPress versions prior to 3.6.
  *
- * @since Twenty Thirteen 1.0
+ * @since Guts 0.0.1
  *
  * @return void
  */
-function twentythirteen_upgrade_notice() {
-	$message = sprintf( __( 'Twenty Thirteen requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'twentythirteen' ), $GLOBALS['wp_version'] );
+function guts_upgrade_notice() {
+	$message = sprintf( __( 'Guts requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] );
 	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
 /**
  * Prevent the Theme Customizer from being loaded on WordPress versions prior to 3.6.
  *
- * @since Twenty Thirteen 1.0
+ * @since Guts 0.0.1
  *
  * @return void
  */
-function twentythirteen_customize() {
-	wp_die( sprintf( __( 'Twenty Thirteen requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'twentythirteen' ), $GLOBALS['wp_version'] ), '', array(
+function guts_customise() {
+	wp_die( sprintf( __( 'Guts requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] ), '', array(
 		'back_link' => true,
 	) );
 }
-add_action( 'load-customize.php', 'twentythirteen_customize' );
+add_action( 'load-customize.php', 'guts_customise' );
 
 /**
  * Prevent the Theme Preview from being loaded on WordPress versions prior to 3.4.
  *
- * @since Twenty Thirteen 1.0
+ * @since Guts 0.0.1
  *
  * @return void
  */
-function twentythirteen_preview() {
+function guts_preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		wp_die( sprintf( __( 'Twenty Thirteen requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'twentythirteen' ), $GLOBALS['wp_version'] ) );
+		wp_die( sprintf( __( 'Guts requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] ) );
 	}
 }
-add_action( 'template_redirect', 'twentythirteen_preview' );
+add_action( 'template_redirect', 'guts_preview' );
