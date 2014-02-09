b4237446c734ca5e0ee3a9bb7e4cfb34cec10ea2
Rose <shmoo@rozeykex.com>
2014-02-09 23:54:54 +0000

    Changed browse happy section to be output by function

--- header.php
+++ header.php
@@ -21,9 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<!--[if lt IE 7]>
-  <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-<![endif]-->
+<?php guts_browse_happy(); ?>
 <div class="l-header-container">
   <header id="site-header">
     <nav class="top-bar" data-topbar role="navigation">
