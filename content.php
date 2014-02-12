22e4d4e77e155c50f0c96698db24aff9a4a85ba4
Rose <shmoo@rozeykex.com>
2014-02-12 13:58:13 +0000

    added icon fi-pencil to edit links

--- content.php
+++ content.php
@@ -23,7 +23,8 @@
 		<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
 		<dd class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'guts' ), __( '1 Comment', 'guts' ), __( '% Comments', 'guts' ) ); ?></dd>
 		<?php endif; ?>
-		<?php edit_post_link( __( 'Edit', 'guts' ), '<dd class="edit-link">', '</dd>' ); ?>
+		<dd class="edit-link"><a class="post-edit-link" href="<?php get_edit_post_link(); ?>"><i class="fi-pencil"></i>Edit</a></dd>
+		<?php //edit_post_link( __( 'Edit', 'guts' ), '<dd class="edit-link"><i class="fi-pencil"></i>', '</dd>' ); ?>
 	</dl>
  </header>
  
