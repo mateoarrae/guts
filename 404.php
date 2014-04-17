b5507cb6c3b23fb6f00195e1d9cfcd76a30d3d27
Rose <rosemckeon@gmail.com>
2014-04-17 12:22:55 +0100

    improved 404 layout to match content-none

--- 404.php
+++ 404.php
@@ -9,23 +9,24 @@
 
 get_header(); ?>
 
-<div class="row">
-	<div class="large-8 columns">
-		<div id="content" class="l-site-content" role="main">
-	
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'guts' ); ?></h1>
-			</header>
-	
-			<div class="page-content">
-				<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'guts' ); ?></h2>
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'guts' ); ?></p>
-			</div><!-- .page-content -->
-	
-		</div><!-- #content -->
+
+<div id="content" class="l-site-content" role="main">
+
+	<div class="row">
+		<div class="small-12 columns">
+		
+				<header class="page-header">
+					<h1 class="page-title"><?php _e( 'Not Found', 'guts' ); ?></h1>
+					<hr />
+				</header>
+				
+				<div class="page-content">
+					<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'guts' ); ?></h2>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'guts' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+		</div>
 	</div>
-	<div class="large-3 offset-1 columns">
-		<?php get_search_form(); ?>
-	</div>
+
 </div>
 <?php get_footer(); ?>
