3c9707f26cf06fb702572f04db258ad7b1b3b5e4
Rose <rosemckeon@gmail.com>
2014-04-22 20:07:46 +0100

    updated to prompt update before 3.8 instead of 3.6

--- includes/back-compat.php
+++ includes/back-compat.php
@@ -2,9 +2,9 @@
 /**
  * Guts back compat functionality - Taken from Twenty Thirteen.
  *
- * Prevents Guts from running on WordPress versions prior to 3.6,
+ * Prevents Guts from running on WordPress versions prior to 3.8,
  * since this theme is not meant to be backward compatible and relies on
- * many new functions and markup changes introduced in 3.6.
+ * many new functions and markup changes introduced in 3.8.
  *
  * @package WordPress
  * @subpackage Guts
@@ -31,26 +31,26 @@ add_action( 'after_switch_theme', 'guts_switch_theme' );
  * Add message for unsuccessful theme switch.
  *
  * Prints an update nag after an unsuccessful attempt to switch to
- * Guts on WordPress versions prior to 3.6.
+ * Guts on WordPress versions prior to 3.8.
  *
  * @since Guts 0.0.1
  *
  * @return void
  */
 function guts_upgrade_notice() {
-	$message = sprintf( __( 'Guts requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] );
+	$message = sprintf( __( 'Guts requires at least WordPress version 3.8. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] );
 	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
 /**
- * Prevent the Theme Customizer from being loaded on WordPress versions prior to 3.6.
+ * Prevent the Theme Customizer from being loaded on WordPress versions prior to 3.8.
  *
  * @since Guts 0.0.1
  *
  * @return void
  */
 function guts_customise() {
-	wp_die( sprintf( __( 'Guts requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] ), '', array(
+	wp_die( sprintf( __( 'Guts requires at least WordPress version 3.8. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] ), '', array(
 		'back_link' => true,
 	) );
 }
@@ -65,7 +65,7 @@ add_action( 'load-customize.php', 'guts_customise' );
  */
 function guts_preview() {
 	if ( isset( $_GET['preview'] ) ) {
-		wp_die( sprintf( __( 'Guts requires at least WordPress version 3.6. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] ) );
+		wp_die( sprintf( __( 'Guts requires at least WordPress version 3.8. You are running version %s. Please upgrade and try again.', 'guts' ), $GLOBALS['wp_version'] ) );
 	}
 }
 add_action( 'template_redirect', 'guts_preview' );
